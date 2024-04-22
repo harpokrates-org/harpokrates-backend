083795edc599dad2efdd9a6514fea4c05d285489
Victor Bravo <vbravo@fi.uba.ar>
2024-04-22 16:43:45 -0300

    factor

--- src/model/FlickrWrapper.js
+++ src/model/FlickrWrapper.js
@@ -134,8 +134,9 @@ class FlickrWrapper {
   
           try {
             const userPhotoIDs = await retry(async () => {
+              const factor = process.env.NODE_ENV === "production" ? 20 : 10;
               return await this._getPhotoIds(user.username, photosPerFavorite);
-            });
+            }, null, {retriesMax: 4, interval: 100, exponential: true, factor: 3, jitter: 100});
             return await this.getUsersWhoHaveFavorited(user.username, userPhotoIDs, photosPerFavorite, depth, mutex, nodes, edges, queue)
           } catch (error) { 
             console.log(util.inspect(error, {showHidden: false, depth: null, colors: true}))
